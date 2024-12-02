c12c006df001d7798d69bf5b6d3c09e260adc8c3
zmudzkik <zmudzki.kamil2@gmail.com>
2024-12-02 23:57:46 +0100

    updated project entity

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -4,7 +4,7 @@ import { Improvement } from './improvement.entity';
 
 @Entity()
 export class Project {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
@@ -16,8 +16,14 @@ export class Project {
   @Column()
   image: string;
 
-  @Column('simple-array') // Store as a comma-separated string
+  @Column('simple-array') // For storing skills as a string array
   skills: string[];
+
+  @Column('simple-array', { nullable: true }) // Optional details array
+  details?: string[];
+
+  @Column('simple-array', { nullable: true }) // Optional technologies array
+  technologies?: string[];
 
   @OneToMany(() => View, (view) => view.project, { cascade: true })
   views: View[];
